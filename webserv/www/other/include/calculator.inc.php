a346801c88d278bfab6a2cf105416be3d62eebcc
Simon Wautelet <swautele@fu-r10-p7.s19.be>
2022-09-26 19:02:37 +0200

    save redo script

--- webserv/www/other/include/calculator.inc.php
+++ webserv/www/other/include/calculator.inc.php
@@ -1,4 +1,16 @@
-num1=12
-oper=add
-num2=2
-submit=
+<?php
+declare (strict_type = 1);
+include ‘class-autoload.inc.php’;
+
+$oper = $_POST[“oper”];
+$num1 = $_POST[“num1"];
+$num2 = $_POST[“num2”];
+
+$calc = new ($oper, (int)$num1, (int)$num2);
+
+try{
+    echo $calc->calculator();
+}
+catch (TypeError $e) {
+    echo “Error! : ” . $e->getMessage();
+}
